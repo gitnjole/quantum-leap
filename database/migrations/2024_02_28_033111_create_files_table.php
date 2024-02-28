14bce7fef967350505354fd5e2ceee2b51cf3c22
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-28 04:36:16 +0100

    feat(migrations): update migrations and add files table

--- database/migrations/2024_02_28_033111_create_files_table.php
+++ database/migrations/2024_02_28_033111_create_files_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('user_id')->constrained()
+            ->onDelete('CASCADE');
+            $table->index('filename');
+            $table->text('path');
+            $table->integer('filesize');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('files');
     }
 };
