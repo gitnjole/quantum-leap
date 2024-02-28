ae68f60cd2c14d1d5fa96b6a41872ea13ee6d20e
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-28 16:57:51 +0100

    feat(login); implement user login method and view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
-{
+{    
+    /**
+     * Method for showing login view
+     *
+     * @return \Illuminate\View\View
+     */
+    public function login(): \Illuminate\View\View
+    {
+        return view('user/login');
+    }
     
+    /**
+     * Method for authenticating the user
+     *
+     * @param Request $request [explicite description]
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function authenticate(Request $request): \Illuminate\Http\RedirectResponse
+    {
+        $formFields = $request->validate([
+            'email' => "required|email",
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You are now logged in!');
+        }
+
+        return back()->withErrors(['email' => 'Invalid credentials'])->onlyInput('email');
+    }
 }
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,7 +1,34 @@
 @extends('../layout')
 @extends('../partials/_nav')
 @section('content')
-<form action="/users">
+<form action="/user/authenticate" method="POST">
+    @csrf
+    <div>
+        <label for="email">
+            Email
+        </label>
+        <input
+            type="email"
+            name="email"
+        />
+        @error('email')
+        <p class="error">{{$message}}</p>
+        @enderror
+    </div>
 
+    <div>
+        <label for="password">
+            Password
+        </label>
+        <input
+            type="password"
+            name="password"
+        />
+        @error('password')
+        <p class="error">{{$message}}</p>
+        @enderror
+    </div>
+
+    <input type="submit">
 </form>
 @endsection
