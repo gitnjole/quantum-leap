11b20da4ac70d17b4bd99b5f0436d5842f3f8954
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-28 16:58:22 +0100

    style: update styling

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,6 @@
 <ul id="home-navigation" class="main-navigation" role="navigation">
     <li class="menu-item menu-item-type-post_type_archive menu-item-object-film menu-item-137 slug-film">
-      <a href="user/login">
+      <a href="login">
         Login
       </a>
     </li>
